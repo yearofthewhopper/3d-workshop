80c51db052b87be98384b1806bfc15fe1ec5a314
ryan spangler <ryan.spangler@gmail.com>
2013-06-19 15:43:06 -0700

    developing boilerplate

--- js/boilerplate.js
+++ js/boilerplate.js
@@ -1,68 +1,92 @@
-var workshop = function() {
-  var renderer, camera;
-  var cameraDistance;
-  var scene, element;
-  var ambient, point;
-  var aspectRatio, windowHalf;
-  var mouse, time;
+var renderer, camera;
+var cameraDistance;
+var scene, element;
+var ambient, point;
+var aspectRatio, windowHalf;
+var mouse, time;
 
-  function init() {
-    element = document.getElementById('workshop');
-    scene = new THREE.Scene();
-    ambient = new THREE.AmbientLight(0x001111);
-    scene.add(ambient);
+function init() {
+  element = document.getElementById('workshop');
+  scene = new THREE.Scene();
+  ambient = new THREE.AmbientLight(0x001111);
+  scene.add(ambient);
 
-    point = new THREE.PointLight(0xffffff);
-    point.position.set(10, 10, 10);
-    scene.add(point);
+  point = new THREE.SpotLight( 0xffffff, 1, 0, Math.PI, 1 );
+  point.position.set( -250, 250, 150 );
+  point.target.position.set( 0, 0, 0 );
 
-    time = 0;
-    mouse = new THREE.Vector2(0, 0);
-    windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
-    aspectRatio = window.innerWidth / window.innerHeight;
-    cameraDistance = 200;
-    camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 1000);
-    camera.position.z = cameraDistance;
-    camera.lookAt(scene.position);
+  // Set shadow parameters for the spotlight.
+  point.castShadow = true;
+  point.shadowCameraNear = 50;
+  point.shadowCameraFar = 1000;
+  point.shadowCameraFov = 50;
+  point.shadowBias = 0.0001;
+  point.shadowDarkness = 0.5;
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    element.appendChild(renderer.domElement);
+  // Larger shadow map size means better looking shadows but impacts performance and texture memory usage.
+  point.shadowMapWidth = 1024;
+  point.shadowMapHeight = 1024;
 
-    document.addEventListener('mousedown', onMouseDown, false);
-    document.addEventListener('mousemove', onMouseMove, false);
-    window.addEventListener('resize', onResize, false);
-  }
-  
-  function onResize() {
-    windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
-    aspectRatio = window.innerWidth / window.innerHeight;
-    camera.aspect = aspectRatio;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+  scene.add(point);
 
-  function onMouseMove(event) {
+  time = 0;
+  mouse = new THREE.Vector2(0, 0);
+  windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+  aspectRatio = window.innerWidth / window.innerHeight;
+  cameraDistance = 200;
+  camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 1000);
+  camera.position.z = cameraDistance;
+  camera.lookAt(scene.position);
 
-  }
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMapEnabled = true;
+  renderer.shadowMapType = THREE.PCFShadowMap;
+  element.appendChild(renderer.domElement);
 
-  function onMouseDown(event) {
+  document.addEventListener('keydown', onKeyDown, false);
+  document.addEventListener('keyup', onKeyUp, false);
+  document.addEventListener('mousedown', onMouseDown, false);
+  document.addEventListener('mousemove', onMouseMove, false);
+  window.addEventListener('resize', onResize, false);
+}
 
-  }
+function onResize() {
+  windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+  aspectRatio = window.innerWidth / window.innerHeight;
+  camera.aspect = aspectRatio;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-  function animate() {
-    requestAnimationFrame(animate);
-    render();
-  }
+function onMouseMove(event) {
 
-  function render() {
-    time++;
-    camera.lookAt(scene.position);
-    renderer.render(scene, camera);
-  }
+}
 
-  window.onload = function() {
-    init();
-    animate();
-  }
-} ();
+function onMouseDown(event) {
+
+}
+
+function onKeyDown(event) {
+
+}
+
+function onKeyUp(event) {
+
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render() {
+  time++;
+  camera.lookAt(scene.position);
+  renderer.render(scene, camera);
+}
+
+window.onload = function() {
+  init();
+  animate();
+}
